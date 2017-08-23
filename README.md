753d243f34a1578b712c9c65ad180745dc509071
sam <s5078345@gmail.com>
2017-08-23 13:12:21 +0800

    Find the reason Why has reshape error

--- README.md
+++ README.md
@@ -6,6 +6,20 @@ Step3 : Get image recognizing result
 
 
 
-test.v3：train_v3 contains 0~9 ten classes, and each has 100 images
+If you want to change the amount of labels, you should change the "onehot" in the code.
+
+
 
 test.v2：train_v2 contains 0~1 two classes, and each has about 100 images
+
+test.v3：train_v3 contains 0~9 ten classes, and each has 100 images
+
+test.v4：train_v4 contains 0~1 two classes, and each has 1000 images
+
+test.v5：try to use another NN architecture
+Now want to shuffle images in advance
+
+test.v6：try to use high resolution images to solve the problem of reshaping
+
+In conclusion, the result is that the image dataset's rewolution is too low, leading to reshape error
+Solution: replace with image for higher resolution
--- kaggle_mnist_alexnet_model.py
+++ kaggle_mnist_alexnet_model.py
@@ -12,6 +12,121 @@ def input_placeholder(image_size, image_channel, label_cnt):
     return inputs, labels, dropout_keep_prob, learning_rate
 
 
+def inference2(inputs, dropout_keep_prob, label_cnt):
+
+  # conv1
+    with tf.name_scope('conv1') as scope:
+        kernel = tf.Variable(tf.truncated_normal([11, 11, 3, 64], dtype=tf.float32,
+                                             stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(inputs, kernel, [1, 4, 4, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[64], dtype=tf.float32),
+                         trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv1 = tf.nn.relu(bias, name=scope)
+        #print_activations(conv1)
+        #parameters += [kernel, biases]
+
+  # lrn1
+    with tf.name_scope('lrn1') as scope:
+        lrn1 = tf.nn.local_response_normalization(conv1,
+                                              alpha=1e-4,
+                                              beta=0.75,
+                                              depth_radius=2,
+                                              bias=2.0)
+
+  # pool1
+    pool1 = tf.nn.max_pool(lrn1,
+                         ksize=[1, 3, 3, 1],
+                         strides=[1, 2, 2, 1],
+                         padding='VALID',
+                         name='pool1')
+    #print_activations(pool1)
+
+  # conv2
+    with tf.name_scope('conv2') as scope:
+        kernel = tf.Variable(tf.truncated_normal([5, 5, 64, 192], dtype=tf.float32,
+                                             stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(pool1, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[192], dtype=tf.float32),
+                         trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv2 = tf.nn.relu(bias, name=scope)
+        #parameters += [kernel, biases]
+        #print_activations(conv2)
+
+  # lrn2
+    with tf.name_scope('lrn2') as scope:
+        lrn2 = tf.nn.local_response_normalization(conv2,
+                                              alpha=1e-4,
+                                              beta=0.75,
+                                              depth_radius=2,
+                                              bias=2.0)
+
+  # pool2
+    pool2 = tf.nn.max_pool(lrn2,
+                         ksize=[1, 3, 3, 1],
+                         strides=[1, 2, 2, 1],
+                         padding='VALID',
+                         name='pool2')
+    #print_activations(pool2)
+
+  # conv3
+    with tf.name_scope('conv3') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 192, 384],
+                                             dtype=tf.float32,
+                                             stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(pool2, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[384], dtype=tf.float32),
+                         trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv3 = tf.nn.relu(bias, name=scope)
+        #parameters += [kernel, biases]
+        #print_activations(conv3)
+
+  # conv4
+    with tf.name_scope('conv4') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 384, 256],
+                                             dtype=tf.float32,
+                                             stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(conv3, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32),
+                         trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv4 = tf.nn.relu(bias, name=scope)
+        #parameters += [kernel, biases]
+        #print_activations(conv4)
+
+  # conv5
+    with tf.name_scope('conv5') as scope:
+        kernel = tf.Variable(tf.truncated_normal([3, 3, 256, 256],
+                                             dtype=tf.float32,
+                                             stddev=1e-1), name='weights')
+        conv = tf.nn.conv2d(conv4, kernel, [1, 1, 1, 1], padding='SAME')
+        biases = tf.Variable(tf.constant(0.0, shape=[256], dtype=tf.float32),
+                         trainable=True, name='biases')
+        bias = tf.nn.bias_add(conv, biases)
+        conv5 = tf.nn.relu(bias, name=scope)
+        #parameters += [kernel, biases]
+        #print_activations(conv5)
+
+
+    # fc layer 1
+    with tf.name_scope('fc1layer'):
+        fc1 = op.fc(conv5, 32, 1.0)
+        fc1 = tf.nn.dropout(fc1, dropout_keep_prob)
+
+    # fc layer 2
+    with tf.name_scope('fc2layer'):
+        #fc2 = op.fc(fc1, 4096, 1.0)
+        fc2 = op.fc(fc1, 16, 1.0)
+        fc2 = tf.nn.dropout(fc2, dropout_keep_prob)
+
+    # fc layer 3 - output
+    with tf.name_scope('fc3layer'):
+        return op.fc(fc2, label_cnt, 1.0, None)	
+		
+		
+	
 def inference(inputs, dropout_keep_prob, label_cnt):
     # todo: change lrn parameters
     # conv layer 1
