906eeaa70fd3e8e365f9d081eccd873d06c3dac1
sam <s5078345@gmail.com>
2017-08-25 13:53:30 +0800

    Implement classifing 10 classes with 20% accuracy

--- kaggle_mnist_alexnet_model.py
+++ kaggle_mnist_alexnet_model.py
@@ -150,20 +150,20 @@ def inference(inputs, dropout_keep_prob, label_cnt):
         conv4 = op.conv(conv3, 3, 384, 1, 1.0)
 
     # conv layer 5
-    #with tf.name_scope('conv5layer'):
-    #    conv5 = op.conv(conv4, 3, 256, 1, 1.0)
-    #    conv5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
+    with tf.name_scope('conv5layer'):
+        conv5 = op.conv(conv4, 3, 256, 1, 1.0)
+        conv5 = tf.nn.max_pool(conv5, ksize=[1, 3, 3, 1], strides=[1, 2, 2, 1], padding='VALID')
 
     # fc layer 1
     with tf.name_scope('fc1layer'):
-        #fc1 = op.fc(conv5, 4096, 1.0)
-        fc1 = op.fc(conv4, 32, 1.0)
+        fc1 = op.fc(conv5, 4096, 1.0)
+        #fc1 = op.fc(conv4, 32, 1.0)
         fc1 = tf.nn.dropout(fc1, dropout_keep_prob)
 
     # fc layer 2
     with tf.name_scope('fc2layer'):
-        #fc2 = op.fc(fc1, 4096, 1.0)
-        fc2 = op.fc(fc1, 16, 1.0)
+        fc2 = op.fc(fc1, 4096, 1.0)
+        #fc2 = op.fc(fc1, 16, 1.0)
         fc2 = tf.nn.dropout(fc2, dropout_keep_prob)
 
     # fc layer 3 - output
